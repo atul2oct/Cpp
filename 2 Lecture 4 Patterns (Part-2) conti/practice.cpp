eb132f9e867f9cf87bedda52791bca4d03d0b5f6
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2025-08-07 17:48:01 +0530

    practice

--- 2 Lecture 4 Patterns (Part-2) conti/practice.cpp
+++ 2 Lecture 4 Patterns (Part-2) conti/practice.cpp
@@ -78,4 +78,23 @@ int main(){
         }
         cout<<endl;
     }
+// 1 
+// 1 2
+// 1   3
+// 1     4
+// 1 2 3 4 5
+    for(int row=0;row<n;row++){
+        for(int col=0;col<row+1;col++){
+            if(col == 0 || col == row){
+                cout<<col+1<<" ";
+            }
+            else if(row == n-1){
+                cout<<col+1<<" ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
 }
