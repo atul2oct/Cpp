9e8d3e2cf57e02d1de5fb372e8c445bcd7291a7c
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2025-08-11 20:08:32 +0530

    practice

--- 3 Lecture 1 Array/practice.cpp
+++ 3 Lecture 1 Array/practice.cpp
@@ -3,17 +3,29 @@ using namespace std;
 
 int main(){
 
-    int arr[10]={1,2};
-    for(int i=0;i<10;i++){
-        cout<<arr[i];
-    }
-    cout<<endl;
+    // int arr[10]={1,2};
+    // for(int i=0;i<10;i++){
+    //     cout<<arr[i];
+    // }
+    // cout<<endl;
 
-    int arr1[10]={1};
-    for(int i=0;i<10;i++){
-        cout<<arr1[i];
-    }
-    cout<<endl;
+    // int arr1[10]={1};
+    // for(int i=0;i<10;i++){
+    //     cout<<arr1[i];
+    // }
+    // cout<<endl;
+
+    // int arr2[10]={0};
+    // for(int i=0;i<10;i++){
+    //     cout<<arr2[i];
+    // }
+    // cout<<endl;
+
+    // int arr3[10];
+    // for(int i=0;i<10;i++){
+    //     cout<<arr3[i];
+    // }
+    // cout<<endl;
 
     int arr2[10]={0};
     for(int i=0;i<10;i++){
@@ -21,11 +33,18 @@ int main(){
     }
     cout<<endl;
 
-    int arr3[10];
-    for(int i=0;i<10;i++){
-        cout<<arr3[i];
-    }
-    cout<<endl;
+    int arr[7];
+    cout<<arr<<endl;
+    cout<<&arr<<endl;
+    
+    int arrA[]={2,3,4,5};
+    int arrB[5]={2,3,4,5};
+    int arrC[10]={2,3,4,5};
 
+    for(int i=0;i<10;i++){
+        cout<<arrC[i]<<" ";
+    }
+
+    int TakeArr[1000];
 
 }
