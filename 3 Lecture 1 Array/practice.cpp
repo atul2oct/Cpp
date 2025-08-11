2dab8a804c0c63d4229456a9f784b04e8d01cdbe
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2025-08-11 19:08:10 +0530

    practice

---
+++ 3 Lecture 1 Array/practice.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[10]={1,2};
+    for(int i=0;i<10;i++){
+        cout<<arr[i];
+    }
+    cout<<endl;
+
+    int arr1[10]={1};
+    for(int i=0;i<10;i++){
+        cout<<arr1[i];
+    }
+    cout<<endl;
+
+    int arr2[10]={0};
+    for(int i=0;i<10;i++){
+        cout<<arr2[i];
+    }
+    cout<<endl;
+
+    int arr3[10];
+    for(int i=0;i<10;i++){
+        cout<<arr3[i];
+    }
+    cout<<endl;
+
+
+}
