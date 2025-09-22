7e06f0fb7d22a214f0cc9ed218938ad54fda226c
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2025-09-22 17:38:11 +0530

    revision

--- 3 Assignments/temp2.cpp
+++ 3 Assignments/temp2.cpp
@@ -1,27 +1,52 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void findMissingElement(int* a,int n){
-
-    for(int i=0;i<n;i++){
+void findMissing(vector<int> & a, int n){
+    
+    // visited method
+   
+    for(int i=0;i<a.size();i++){
         int index = abs(a[i]);
-        if(a[index-1] > 0){
-            a[index-1] *= -1;
+
+        if(a[index - 1] > 0){
+            a[index - 1] *= -1;
+        }
+
+    }
+
+    for(int i=0;i<a.size();i++){
+        if(a[i] > 0)
+            cout<<i+1<<" ";        
+    }       
+
+}
+
+void findMissing2(vector<int> & a, int n){
+    
+    //  sort swap
+   
+    // sort part
+    int i=0;
+    while(i < n){
+        int index=a[i] - 1;
+        if(a[i] != a[index]){
+            swap(a[i],a[index]);
+        }
+        else{
+            i++;
         }
     }
 
-    for(int i=0;i<n;i++){
-        if(a[i] > 0){
-            cout<< i+1 << " ";
-        }
-    }
+    for(int i=0;i<a.size();i++){
+        if(a[i] != i+1)
+            cout<<i+1<<" ";        
+    }       
 
 }
 
 int main(){
-    int arr[] = {1,3,5,3,4};
-    // int arr[] = {1,3,3,3,4};
-    int n = sizeof(arr)/sizeof(int);// to find no. of element
-    findMissingElement(arr,n);
+    vector<int> arr {1,3,5,3,4};
+    findMissing2(arr,arr.size());
     return 0;
 }
