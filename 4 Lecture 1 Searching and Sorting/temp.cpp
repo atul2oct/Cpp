d4e5de211a942f7c0f60b58b10685f1c8be6df60
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2025-12-07 22:46:44 +0530

    search and sort 1 complted

--- 4 Lecture 1 Searching and Sorting/temp.cpp
+++ 4 Lecture 1 Searching and Sorting/temp.cpp
@@ -1,68 +1,47 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-int firstOccurence(vector <int> arr,int size,int target){
-    int start=0;
-    int end=size-1;
-    int mid=start+(end-start)/2;
-    int ans=-1;
-
-    while(start <= end){
-        // target found
-        if(arr[mid] == target){
-            ans=mid;
-            end=mid-1;
-        }else if(arr[mid] < target){
-            // search right 
-            start=mid+1;
+int Peak(vector<int>& arr){
+    int start =0;
+    int end = arr.size()-1;
+    int mid = start + (end-start)/2;
+    while(start < end){
+        if(arr[mid] < arr[mid + 1]){
+            start = mid + 1;
         }else{
-            // search left
-            end=mid-1;
+            end = mid;
         }
-        mid=start+(end-start)/2;
+        mid = start + (end-start)/2;
     }
-    return ans;
+    return start;
 }
-int lasttOccurence(vector <int> arr,int size,int target){
-    int start=0;
-    int end=size-1;
-    int mid=start+(end-start)/2;
-    int ans=-1;
 
-    while(start <= end){
-        // target found
-        if(arr[mid] == target){
-            ans=mid;
-            start=mid+1;
-        }else if(arr[mid] < target){
-            // search right 
-            start=mid+1;
+int Peak2(vector<int>& arr){
+    int start =0;
+    int end = arr.size()-1;
+    int mid = start + (end-start)/2;
+    while(start < end){
+        // if(arr[mid] < arr[mid + 1]){
+        //     start = mid + 1;
+        // }else{
+        //     end = mid;
+        // }
+        if(mid > 0 && mid < arr.size()-1 && arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) return mid;
+
+        if(mid > 0 && arr[mid] < arr[mid+1] || arr[mid] > arr[mid-1]){
+            start = mid + 1;
         }else{
-            // search left
-            end=mid-1;
+            end = mid - 1;
         }
-        mid=start+(end-start)/2;
+        mid = start + (end-start)/2;
     }
-    return ans;
-}
-int total(vector <int> arr,int size,int target){
-    int ans1=firstOccurence(arr,size,target);
-    int ans2=lasttOccurence(arr,size,target);
-    return ans2-ans1+1;
+    return -1;
 }
 int main(){
-    vector <int> arr{0 ,0 ,1 ,1 ,2, 2, 2 ,2};
-    int target=2;
-    cout<<total(arr,arr.size(),target)<<endl;
-
-    // int indexOfTarget=total(arr,arr.size(),target);
-
-    // if(indexOfTarget == -1){
-    //     cout<<"Target not found"<<endl;
-    // }
-    // else{
-    //     cout<<"Found at index: "<<indexOfTarget<<endl;
-    // }
+    vector<int> arr{0,1,10,5,2};
+    cout<<Peak(arr)<<endl;
+    
     return 0;
 }
