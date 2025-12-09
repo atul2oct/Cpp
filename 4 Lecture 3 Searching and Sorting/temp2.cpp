c9004dde7bedece8dc9647679ab4807322047412
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2025-12-09 11:18:05 +0530

    nearly sorted array binarysearch

---
+++ 4 Lecture 3 Searching and Sorting/temp2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch(vector<int> arr, int target){
+    int start = 0;
+    int end = arr.size()-1;
+    int mid = start + (end - start)/2;
+    while(start <= end){
+        if(arr[mid] == target){
+            return mid;
+        }
+        if(arr[mid-1] == target){
+            return mid-1;
+        }
+        if(arr[mid+1]==target){
+            return mid+1;
+        }
+
+        if(arr[mid] > target){
+            // saearch left
+            end=mid-2;
+        }else{
+            // search right arr[mid]<target
+            start=mid+2;
+        }
+        mid=start+(end-start)/2;
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> arr{10,3,40,20,50,80,70};
+    int target=70;
+    cout<<"index of "<<binarySearch(arr,target)<<endl;
+    return 0;
+}
