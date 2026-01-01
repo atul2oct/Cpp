2d31c30384ad833807a719f4b5312a3712cbd5ed
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-01-01 11:16:54 +0530

    Update Assignment2.cpp

--- 4 Assignments/Assignment2.cpp
+++ 4 Assignments/Assignment2.cpp
@@ -2,8 +2,8 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
-vector<int> KclosestDiff(vector<int> arr, int k, int x){
+// approach 1 2 pointers
+vector<int> KclosestDiff1(vector<int> arr, int k, int x){
     // two pointer approach
     int low = 0, high =arr.size()-1;
     // k is no of arr element output needs
@@ -22,12 +22,54 @@ vector<int> KclosestDiff(vector<int> arr, int k, int x){
     // return ans;
     return vector<int> (arr.begin()+low,arr.begin()+high+1);
 }
+// binary search +  2 pointer
+
+int lowerbound(vector<int> arr, int x){
+    int start = 0, end = arr.size()-1;
+    // arr =[3,5,8,10] x=15 k=2 beacuse and 10 kabhi nhi milega
+    int ans = end;
+    while(start <= end){
+        int mid = (start+end)/2;
+        if(arr[mid] >= x){
+            ans=mid;
+            end=mid-1;
+        }
+        else if(arr[mid] > x){
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+    return ans;
+}
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int high = lowerbound(arr,x);
+    int low = high-1;
+    while(k--){
+        // arr=[1,1,2,3,4,5] k=4 x=-1 if(low is in negative so overflow)
+        if(low < 0){
+            high++;
+        }else if(high >= arr.size()){
+            // arr=[1,2,3,4,5] k=4 x=6 if(high is overflow)
+            low--;
+        }else if(x - arr[low] > arr[high] - x){
+            high++;
+        }else{
+            low--;
+        }
+    }
+    return vector<int> (arr.begin()+low+1,arr.begin()+high);
+    
+}
+
+// leetcode best approach with binary search
 int main(){
     vector<int> arr {1,2,3,4,5};
     int k=44;
     int x=3;
     vector<int> ans;
-    ans = KclosestDiff(arr,k,x);
+    // ans = KclosestDiff(arr,k,x);
+    ans = findClosestElements(arr,k,x);
     for(int i=0;i<ans.size();i++){
         cout<<ans[i]<<" ";
     }
