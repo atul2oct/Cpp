861b1b1b952ffcf5d54b589c44c63d678a46449a
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-07-02 01:40:25 +0530

    lect 2

--- 5 Lecture 2 Char Arrays and Strings/revision.cpp
+++ 5 Lecture 2 Char Arrays and Strings/revision.cpp
@@ -1,3 +1,23 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+string removeDuplicate(string str){
+    string ans = "";
+    int i=0;
+    while(i < str.length()){
+        if(ans.length() > 0 && ans[ans.length()-1] == str[i]){
+            ans.pop_back();
+        }else{
+            ans.push_back(str[i]);
+        }
+        i++;
+    }
+    return ans;
+}
+int main(){
+    string str="aabbbcc";
+    string ans=removeDuplicate(str);
+    cout<<"new string without adjacent duplicates are: "<<ans<<endl;
+
+}
