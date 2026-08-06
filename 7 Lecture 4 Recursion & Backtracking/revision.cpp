a27f890c676237452139429d34de821a69e6a85c
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-08-07 01:36:08 +0530

    max no of segments cut

--- 7 Lecture 4 Recursion & Backtracking/revision.cpp
+++ 7 Lecture 4 Recursion & Backtracking/revision.cpp
@@ -4,50 +4,78 @@
 using namespace std;
 
 int coinChange(vector<int>& arr,int target){
+    // base case 1
     if(target == 0){
         return 0;
     }
+    // base case 2
     if(target < 0){
         return INT_MAX;
     }
-
-    int mini = INT_MAX;
-
+    int mini=INT_MAX;
     for(int i=0;i<arr.size();i++){
-        int ans = coinChange(arr,target-arr[i]);
-
+        int ans=coinChange(arr,target-arr[i]);
         if(ans != INT_MAX){
-            mini = min(mini,ans+1);
+            mini=min(mini,ans+1);
         }
     }
     return mini;
 }
-int coinChange2(vector<int>& arr,int& target,int output){
+
+int coinChange2(vector<int>& arr,int target,int output){
+    // base case 1
     if(target == output){
         return 0;
     }
+    // base case 2
     if(target < output){
         return INT_MAX;
     }
-
-    int mini = INT_MAX;
-
+    int mini=INT_MAX;
     for(int i=0;i<arr.size();i++){
-        int ans = coinChange2(arr,target,output+arr[i]);
-
+        int ans=coinChange2(arr,target,output+arr[i]);
         if(ans != INT_MAX){
-            mini = min(mini,ans+1);
+            mini=min(mini,ans+1);
+
+
+
         }
     }
     return mini;
 }
+int cutSegments(int n, int x, int y, int z){
+    if(n == 0){
+        return 0;
+    }
 
+    if(n < 0){
+        return INT_MIN;
+    }
+
+    int ans1 = cutSegments(n-x,x,y,z) + 1;
+    int ans2 = cutSegments(n-y,x,y,z) + 1;
+    int ans3 = cutSegments(n-z,x,y,z) + 1;
+
+    int ans = max(ans1,max(ans2,ans3));
+    return ans;
+}
 int main(){
-    vector<int> arr{1,2,3};
-    int target=7;
+    // vector<int> arr{1,2,3};
+    // int target=7;
 
     // int ans=coinChange(arr,target);
-    int ans=coinChange2(arr,target,0);
-    cout<<"Minimum no. of ways are: "<<ans<<endl;
+    // // int ans=coinChange2(arr,target,0);
+    // cout<<"Minimum no. of ways are: "<<ans<<endl;
+
+    int n=8;
+    int x=3;
+    int y=3;
+    int z=3;
+
+    int ans=cutSegments(n,x,y,z);
+    if(ans < 0)
+        ans=0;
+    cout<<"Max no of segments are: "<<ans<<endl;
+    
     return 0;
 }
